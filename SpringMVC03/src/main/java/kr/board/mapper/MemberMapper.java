e73cd984cf811bd2998730e18bfeee5f4d594e95
강준석 <@junseok>
2023-01-28 20:41:21 +0900

    mvc03 회원가입구현(아이디중복확인,비밀번호확인,내용입력완료여부,나이입력여부)

---
+++ SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
@@ -0,0 +1,13 @@
+package kr.board.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.board.entity.Member;
+
+@Mapper // Mybatis API
+public interface MemberMapper {
+		
+	public Member registerCheck(String memID);
+	public int register(Member m); //회원등록(성공1, 실패0)
+
+}
